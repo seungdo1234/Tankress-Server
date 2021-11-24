f5b5d7ebe14dbd8aed1ca7bb9bcdbbeacf253608
seungdo1234 <wltmdeh12@naver.com>
2021-11-24 21:14:05 +0900

    dadadada

--- Tankress Server/Tankress Server.cpp
+++ Tankress Server/Tankress Server.cpp
@@ -15,18 +15,22 @@ using namespace std;
 #pragma warning(disable:4996)
 HANDLE  mutex;
 unsigned  threadID;
-
-int Sermap[10][10] = { 0, };
-int Coordinate1[6] = { 0, };
-int Coordinate2[6] = { 0, };
-int Coordinate3[6] = { 0, };
+SOCKET servsoc;
+SOCKET clisoc;
+int clientNumber = 0;
+int player[4] = { 0, };
 int allClientSocket[100];
-int we[1] = { 0, };
-int y = 4;
-int index[3] = { 0 , };
+struct sockaddr_in clientAddress;
+int clientAddressSize = sizeof(clientAddress);
 class SK {
 public:
-
+	int Sermap[10][10] = { 0, };
+	int Coordinate1[6] = { 0, };
+	int Coordinate2[6] = { 0, };
+	int Coordinate3[6] = { 0, };
+	int we[1] = { 0, };
+	int y = 4;
+	int index[3] = { 0 , };
 	virtual void  Ser_gs(void) = 0;
 	SK();
 };
@@ -55,18 +59,12 @@ SK::SK() {
 
 class SK_C :public SK {
 private:
-	int clientNumber = 0;
-	int player[4] = { 0, };
 	WSADATA wsdata;
 	sockaddr_in serverAddress;
-	struct sockaddr_in clientAddress;
-	int clientAddressSize = sizeof(clientAddress);
-	SOCKET servsoc;
-	SOCKET clisoc;
 public:
 	void Ser_open();
 	void print();
-	static unsigned __stdcall con1(void* arg);
+	static unsigned __stdcall con(void* arg);
 	void Ser_gs(void);
 	~SK_C();
 };
@@ -94,6 +92,53 @@ void SK_C::Ser_open() {
 		return;
 	}
 	cout << "\n  탱크리스 서버가 만들어졌습니다.\n" << endl;
+	cout << "\n  게임을 시작할려면 ENTER를 누르시오.   (최소인원 1명) \n\n";
+	unsigned long thread;
+	thread = _beginthreadex(NULL, 0, con, (void*)Sermap, 0, &threadID);
+	while (clientNumber == 0) {
+	
+	}
+	while (1) {
+		int tmp = _getch();
+		if (tmp == 13) {
+			break;
+		}
+	}
+	while (1) {
+		cout << "  게임모드를 설정하시오. 1) 비안개모드 2) 안개모드 : ";
+		cin >> index[0];
+		if ((index[0] == 1 && index[0] != 2) || (index[0] != 1 && index[0] == 2)) {
+			break;
+		}
+		else {
+			cout << "  게임모드는 1) 과 2) 중에 다시 선택하세요. \n";
+		}
+	}
+	while (1) {
+		cout << "  탱크이동시간을 설정하시오 (300~500) : ";
+		cin >> index[1];
+		if (index[1] >= 300 && index[1] <= 500) {
+			break;
+		}
+		else {
+			cout << "  탱크이동시간은 300~500 사이 입니다. 다시 설정하세요. \n";
+		}
+	}
+	index[2] = clientNumber;
+	system("cls");
+	for (int i = 0; i < 3; i++) {
+		index[i] = ntohl(index[i]);
+	}
+	for (int i = 0; i < 3; i++) {
+		send(allClientSocket[i], (char*)index, sizeof(index), 0);
+	}
+	for (int i = 0; i < 3; i++) {
+		index[i] = ntohl(index[i]);
+	}
+	cout << "\n  탱크리스를 시작하겠습니다. " << endl;
+	Sleep(3000);
+}
+unsigned __stdcall SK_C::con(void* arg) {
 	while (1) {
 		allClientSocket[clientNumber] = accept(servsoc, (struct sockaddr*)&clientAddress, &clientAddressSize);
 		cout << "  " << clientNumber + 1 << "번째 플레이어가 접속 했습니다.  서버 인원 : [" << clientNumber + 1 << "/3]" << endl;
@@ -118,69 +163,33 @@ void SK_C::Ser_open() {
 		}
 		send(allClientSocket[clientNumber], (char*)player, sizeof(player), 0);
 		clientNumber++;
-		if (clientNumber == 3) {
-			cout << "  게임시작 인원이 충족되었습니다. " << endl;
-			break;
-		}
 	}
-	while (1) {
-		cout << "  게임모드를 설정하시오. 1) 비안개모드 2) 안개모드 : ";
-		cin >> index[0];
-		if ((index[0] == 1 && index[0] != 2) || (index[0] != 1 && index[0] == 2)) {
-			break;
-		}
-		else {
-			cout << "  게임모드는 1) 과 2) 중에 다시 선택하세요. \n";
-		}
-	}
-	while (1) {
-		cout << "  탱크이동시간을 설정하시오 (300~500) : ";
-		cin >> index[1];
-		if (index[1] >= 300 && index[1] <= 500) {
-			break;
-		}
-		else {
-			cout << "  탱크이동시간은 300~500 사이 입니다. 다시 설정하세요. \n";
-		}
-	}
-	system("cls");
-	for (int i = 0; i < 2; i++) {
-		index[i] = ntohl(index[i]);
-	}
-	for (int i = 0; i < 3; i++) {
-		send(allClientSocket[i], (char*)index, sizeof(index), 0);
-	}
-	for (int i = 0; i < 2; i++) {
-		index[i] = ntohl(index[i]);
-	}
-	cout << "\n  탱크리스를 시작하겠습니다. " << endl;
-	Sleep(3000);
 }
-unsigned __stdcall SK_C::con1(void* arg) {
+void SK_C::Ser_gs(void) {
+	Ser_open();
+
 	while (1) {
 		y = recv(allClientSocket[0], (char*)Coordinate1, sizeof(Coordinate1), 0);
-		WaitForSingleObject(mutex, INFINITE);
-		if (y == -1) return 1;
+		if (y == -1) return;
 		if (kbhit()) {
 			int tmp = _getch();
 			if (tmp == 77 || tmp == 109) {
 				if (index[0] == 1) {
-					we[0] = ntohl(2);
-					Coordinate1[4] = ntohl(2);
 					index[0] = 2;
 					tmp = 0;
 				}
 				else if (index[0] == 2) {
-					we[0] = ntohl(1);
-					Coordinate1[4] = ntohl(1);
 					index[0] = 1;
 					tmp = 0;
 				}
 			}
 		}
-		send(allClientSocket[0], (char*)we, sizeof(we), 0);
-		send(allClientSocket[1], (char*)Coordinate1, sizeof(Coordinate1), 0);
-		send(allClientSocket[2], (char*)Coordinate1, sizeof(Coordinate1), 0);
+		if (index[2] > 1) {
+			send(allClientSocket[1], (char*)Coordinate1, sizeof(Coordinate1), 0);
+		}
+		if (index[2] > 2) {
+			send(allClientSocket[2], (char*)Coordinate1, sizeof(Coordinate1), 0);
+		}
 		for (int i = 0; i < 4; i++) {
 			Coordinate1[i] = ntohl(Coordinate1[i]);
 		}
@@ -221,12 +230,14 @@ unsigned __stdcall SK_C::con1(void* arg) {
 		if (Coordinate1[2] != 0) {
 			Sermap[Coordinate1[0]][Coordinate1[1]] = Coordinate1[2];
 		}
-		ReleaseMutex(mutex);
-		y = recv(allClientSocket[1], (char*)Coordinate2, sizeof(Coordinate2), 0);
-		WaitForSingleObject(mutex, INFINITE);
-		if (y == -1) return 1;
-		send(allClientSocket[0], (char*)Coordinate2, sizeof(Coordinate2), 0);
-		send(allClientSocket[2], (char*)Coordinate2, sizeof(Coordinate2), 0);
+		if (index[2] > 1) {
+			y = recv(allClientSocket[1], (char*)Coordinate2, sizeof(Coordinate2), 0);
+			if (y == -1) return;
+			send(allClientSocket[0], (char*)Coordinate2, sizeof(Coordinate2), 0);
+		}
+		if (index[2] > 2) {
+			send(allClientSocket[2], (char*)Coordinate2, sizeof(Coordinate2), 0);
+		}
 		for (int i = 0; i < 4; i++) {
 			Coordinate2[i] = ntohl(Coordinate2[i]);
 		}
@@ -274,12 +285,12 @@ unsigned __stdcall SK_C::con1(void* arg) {
 			}
 			else Sermap[Coordinate2[0]][Coordinate2[1]] = Coordinate2[2];
 		}
-		ReleaseMutex(mutex);
-		y = recv(allClientSocket[2], (char*)Coordinate3, sizeof(Coordinate3), 0);
-		WaitForSingleObject(mutex, INFINITE);
-		if (y == -1) return 1;
-		send(allClientSocket[0], (char*)Coordinate3, sizeof(Coordinate3), 0);
-		send(allClientSocket[1], (char*)Coordinate3, sizeof(Coordinate3), 0);
+		if (index[2] > 2) {
+			y = recv(allClientSocket[2], (char*)Coordinate3, sizeof(Coordinate3), 0);
+			if (y == -1) return;
+			send(allClientSocket[0], (char*)Coordinate3, sizeof(Coordinate3), 0);
+			send(allClientSocket[1], (char*)Coordinate3, sizeof(Coordinate3), 0);
+		}
 		for (int i = 0; i < 4; i++) {
 			Coordinate3[i] = ntohl(Coordinate3[i]);
 		}
@@ -326,103 +337,65 @@ unsigned __stdcall SK_C::con1(void* arg) {
 			}
 			else Sermap[Coordinate3[0]][Coordinate3[1]] = Coordinate3[2];
 		}
-		ReleaseMutex(mutex);
-		WaitForSingleObject(mutex, INFINITE);
-		system("cls");
-		if (index[0] == 1) {
-			cout << "\n    비안개 모드 \n" << endl;
-			for (int i = 0; i < 10; i++) {
-				for (int j = 0; j < 10; j++) {
-					if (Sermap[i][j] == 0) cout << "□";
-					else if (Sermap[i][j] == 1) cout << "■";
-					else if (Sermap[i][j] == 2) cout << "▶";
-					else if (Sermap[i][j] == 3) cout << "◀";
-					else  if (Sermap[i][j] == 4) cout << "▲";
-					else if (Sermap[i][j] == 5) cout << "▼";
-					else if (Sermap[i][j] == 6) {
-						cout << "ㅡ";
-						Sermap[i][j] = 0;
-					}
-					else if (Sermap[i][j] == 7) {
-						cout << "ㅣ";
-						Sermap[i][j] = 0;
-					}
-				}
-				cout << endl;
-			}
-		}
-		if (index[0] == 2) {
-			cout << "\n    안개 모드 \n" << endl;
-			for (int i = 0; i < 10; i++) {
-				for (int j = 0; j < 10; j++) {
-					if (Sermap[i][j] == 1) cout << "■";
-					else cout << "□";
-				}
-				cout << endl;
-			}
-		}
-		cout << "\n\n모드를 변경 할려면 m키를 누르시오. \n" << endl;
-		Sermap[Coordinate2[0]][Coordinate2[1]] = 0;
-		Sermap[Coordinate1[0]][Coordinate1[1]] = 0;
-		Sermap[Coordinate3[0]][Coordinate3[1]] = 0;
-		ReleaseMutex(mutex);
-	}
-
-}
-void SK_C::Ser_gs(void) {
-	Ser_open();
-	unsigned long thread;
-	thread = _beginthreadex(NULL, 0, con1, (void*)Sermap, 0, &threadID);
-	while (1) {
-		if (y == -1) return;
+		print();
 	}
 }
 void SK_C::print() {
 	system("cls");
 	if (index[0] == 1) {
 		cout << "\n    비안개 모드 \n" << endl;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (Sermap[i][j] == 0) cout << "□";
+				else if (Sermap[i][j] == 1) cout << "■";
+				else if (Sermap[i][j] == 2) cout << "▶";
+				else if (Sermap[i][j] == 3) cout << "◀";
+				else  if (Sermap[i][j] == 4) cout << "▲";
+				else if (Sermap[i][j] == 5) cout << "▼";
+				else if (Sermap[i][j] == 6) {
+					cout << "ㅡ";
+					Sermap[i][j] = 0;
+				}
+				else if (Sermap[i][j] == 7) {
+					cout << "ㅣ";
+					Sermap[i][j] = 0;
+				}
+			}
+			cout << endl;
+		}
 	}
 	if (index[0] == 2) {
 		cout << "\n    안개 모드 \n" << endl;
-	}
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			if (Sermap[i][j] == 0) cout << "□";
-			else if (Sermap[i][j] == 1) cout << "■";
-			else if (Sermap[i][j] == 2) cout << "▶";
-			else if (Sermap[i][j] == 3) cout << "◀";
-			else  if (Sermap[i][j] == 4) cout << "▲";
-			else if (Sermap[i][j] == 5) cout << "▼";
-			else if (Sermap[i][j] == 6) {
-				cout << "ㅡ";
-				Sermap[i][j] = 0;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (Sermap[i][j] == 1) cout << "■";
+				else cout << "□";
 			}
-			else if (Sermap[i][j] == 7) {
-				cout << "ㅣ";
-				Sermap[i][j] = 0;
-			}
+			cout << endl;
 		}
-		cout << endl;
 	}
 	cout << "\n\n모드를 변경 할려면 m키를 누르시오. \n" << endl;
-	Sleep(index[1]);
-//	Sermap[Coordinate2[0]][Coordinate2[1]] = 0;
-//	Sermap[Coordinate1[0]][Coordinate1[1]] = 0;
-//	Sermap[Coordinate3[0]][Coordinate3[1]] = 0;
+	Sermap[Coordinate2[0]][Coordinate2[1]] = 0;
+	if (index[2] > 1) {
+		Sermap[Coordinate1[0]][Coordinate1[1]] = 0;
+	}
+	if (index[2] > 2) {
+		Sermap[Coordinate3[0]][Coordinate3[1]] = 0;
+	}
 }
 SK_C ::~SK_C() {
 	system("cls");
-	if (Coordinate1[2] != 0 ) {
+	if (Coordinate1[2] != 0) {
 		cout << "\n\n  1번 플레이어가 우승했습니다. " << endl;
 	}
-	else if (Coordinate2[2] != 0 ) {
+	else if (Coordinate2[2] != 0) {
 		cout << "\n\n  2번 플레이어가 우승했습니다. " << endl;
 	}
-	else if (Coordinate3[2] != 0 ) {
+	else if (Coordinate3[2] != 0) {
 		cout << "\n\n  3번 플레이어가 우승했습니다. " << endl;
 	}
 	cout << "\n  서버가 닫힙니다......... \n\n";
-	
+
 	Sleep(3000);
 }
 int main() {
